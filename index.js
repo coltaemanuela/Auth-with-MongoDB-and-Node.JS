b83a4c9525f501999c86cf7029ab07a5e27387c3
DESKTOP-6HEB7R2\Emanuela <emanuela.colta@mobiversal.com>
2017-05-10 13:27:45 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+//-------------------------------------------------------------Modules--------------------------------------------------------------
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var path = require('path');
+var app = express();
+//---------------------------------------------------------------Set up view engine---------------------------------------------------
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//------------------------------------------------Routes---------------------------------------------------------------------------
+app.get('/', function (req, res) {
+    res.send('hello');
+});
+
+//------------------------------------------------Server-------------------------------------------------------------------------
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
