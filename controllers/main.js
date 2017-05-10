10cc4c64bf66bfb2b905c8e674807b51776b1520
DESKTOP-6HEB7R2\Emanuela <emanuela.colta@mobiversal.com>
2017-05-10 16:10:43 +0300

    remove comments

--- controllers/main.js
+++ controllers/main.js
@@ -5,26 +5,19 @@ var bcrypt = require('bcrypt');
 var router = express.Router();
 var User=require('../models/users_model');
 
-
 //-------------------------------------------------------------Routes--------------------------------------------------------------
-router.get('/', function(req, res, next) {
+ router.get('/', function(req, res, next) {
   res.render('index', { user:'emma', title: 'My Mongo App' });
-});
+ });
 
-router.get('/signup',function(req,res){
+ router.get('/signup',function(req,res){
   res.render('signup', { title: 'Sign up' });
-}); 
+ ); 
 
-router.post('/signup',function(req,res){  
-     //var user= new User({
-     //username: req.body.username,
-     //email: req.body.email,
-     //password:req.body.password
-     // });
+ router.post('/signup',function(req,res){  
      
     const saltRounds = 10;
     const myPlaintextPassword = req.body.password;
-   // const someOtherPlaintextPassword = 'altaparola';
     
     bcrypt.genSalt(saltRounds, function(err, salt) {
         bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
